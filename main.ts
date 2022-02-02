56dc51c07f01626a551c3cd78dd483d481118dea
Patrick O'Brien <34171640+patrickobriain@users.noreply.github.com>
2022-02-02 11:25:55 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -5,6 +5,10 @@ input.onButtonPressed(Button.B, function () {
     basic.showNumber(randint(1, 17))
 })
 input.onGesture(Gesture.Shake, function () {
-    basic.showIcon(IconNames.SmallDiamond)
-    basic.showIcon(IconNames.Diamond)
+    for (let index = 0; index < 5; index++) {
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.pause(2000)
+        basic.showIcon(IconNames.Diamond)
+        basic.pause(2000)
+    }
 })
